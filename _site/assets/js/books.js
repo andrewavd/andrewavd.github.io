629c9fcf9785d94270925e48bcea082cd7f43b63
andrewavd <andyavd01@gmail.com>
2019-01-27 19:29:05 -0500

    updates

--- _site/assets/js/books.js
+++ _site/assets/js/books.js
@@ -1,35 +1,34 @@
 function handleResponse(response) {
-  document.getElementById("content").innerHTML = '';
-  if (response.totalItems == 0) {
-    document.getElementById('content').innerHTML = 'No Results Found';
+  "use strict";
+  document.getElementById("content").innerHTML = "";
+  if (response.totalItems === 0) {
+    document.getElementById("content").innerHTML = "No Results Found";
   }
-  for (var i = 0; i < response.items.length; i++) {
-    var item = response.items[i];
-
-    var book = document.createElement('div');
-    var titleLink = document.createElement('a');
+  response.items.forEach(function (item) {
+    var book = document.createElement("div");
+    var titleLink = document.createElement("a");
     titleLink.href = item.volumeInfo.infoLink;
-    titleLink.target = '_blank';
-    var title = document.createElement('h4');
+    titleLink.target = "_blank";
+    var title = document.createElement("h4");
     title.innerHTML = item.volumeInfo.title;
-    var authorLink = document.createElement('a');
+    var authorLink = document.createElement("a");
     authorLink.href = item.volumeInfo.infolink;
-    authorLink.target = '_blank';
-    var author = document.createElement('div');
+    authorLink.target = "_blank";
+    var author = document.createElement("div");
     author.innerHTML = item.volumeInfo.authors;
-    var publisherLink = document.createElement('a');
+    var publisherLink = document.createElement("a");
     publisherLink.href = item.volumeInfo.infoLink;
-    publisherLink.target = '_blank';
-    var publisher = document.createElement('div');
+    publisherLink.target = "_blank";
+    var publisher = document.createElement("div");
     publisher.innerHTML = item.volumeInfo.publisher;
-    var imgLink = document.createElement('a');
+    var imgLink = document.createElement("a");
     imgLink.href = item.volumeInfo.infoLink;
-    imgLink.target = '_blank';
-    var image = document.createElement('IMG');
+    imgLink.target = "_blank";
+    var image = document.createElement("IMG");
     image.src = item.volumeInfo.imageLinks.thumbnail;
     image.alt = "No Image Available";
 
-    book.appendChild(document.createElement('hr'));
+    book.appendChild(document.createElement("hr"));
     titleLink.appendChild(title);
     book.appendChild(titleLink);
     authorLink.appendChild(author);
@@ -39,20 +38,21 @@ function handleResponse(response) {
     imgLink.appendChild(image);
     book.appendChild(imgLink);
     document.getElementById("content").appendChild(book);
-  }
+  });
 }
 
 function bookSearch() {
-  var script = document.createElement('script');
+  "use strict";
+  var script = document.createElement("script");
   var searchCriteria = document.getElementById("bookInput").value;
-  script.src="https://www.googleapis.com/books/v1/volumes?q=" + searchCriteria + "&callback=handleResponse"
+  script.src = "https://www.googleapis.com/books/v1/volumes?q=" + searchCriteria + "&callback=handleResponse";
   document.body.appendChild(script);
 }
 
-document.getElementById("bookInput")
-.addEventListener("keyup", function(event) {
+document.getElementById("bookInput").addEventListener("keyup", function (event) {
+  "use strict";
   event.preventDefault();
   if (event.keyCode === 13) {
-    document.getElementById("bookSearch").click();
+      document.getElementById("bookSearch").click();
   }
 });
