7ccd30d5efc47896f235dcb4e0a498a2c632c81d
Andy Holmes <andyavd01@gmail.com>
2019-02-11 14:16:41 -0500

    refactoring

--- assets/js/books.js
+++ assets/js/books.js
@@ -1,58 +1,90 @@
-function handleResponse(response) {
-  "use strict";
-  document.getElementById("content").innerHTML = "";
-  if (response.totalItems === 0) {
-    document.getElementById("content").innerHTML = "No Results Found";
+function clearResults(section) {
+  while (section.firstChild) {
+    section.removeChild(section.firstChild);
   }
+}
+
+function displayResults(response, section) {
+  "use strict";
+  const OPENNEWTAB = "_blank";
+
+  clearResults(section);
+
   response.items.forEach(function (item) {
     var book = document.createElement("div");
+
     var titleLink = document.createElement("a");
     titleLink.href = item.volumeInfo.infoLink;
-    titleLink.target = "_blank";
+    titleLink.target = OPENNEWTAB;
+    titleLink.textContent = item.volumeInfo.title
     var title = document.createElement("h4");
-    title.innerHTML = item.volumeInfo.title;
+
     var authorLink = document.createElement("a");
-    authorLink.href = item.volumeInfo.infolink;
-    authorLink.target = "_blank";
+    authorLink.href = item.volumeInfo.infoLink;
+    authorLink.target = OPENNEWTAB;
+    authorLink.textContent = item.volumeInfo.authors
     var author = document.createElement("div");
-    author.innerHTML = item.volumeInfo.authors;
+
     var publisherLink = document.createElement("a");
     publisherLink.href = item.volumeInfo.infoLink;
-    publisherLink.target = "_blank";
+    publisherLink.target = OPENNEWTAB;
+    publisherLink.textContent = item.volumeInfo.publisher
     var publisher = document.createElement("div");
-    publisher.innerHTML = item.volumeInfo.publisher;
-    var imgLink = document.createElement("a");
-    imgLink.href = item.volumeInfo.infoLink;
-    imgLink.target = "_blank";
-    var image = document.createElement("IMG");
+
+    var imageLink = document.createElement("a");
+    imageLink.href = item.volumeInfo.infoLink;
+    imageLink.target = OPENNEWTAB;
+    var image = document.createElement("img");
     image.src = item.volumeInfo.imageLinks.thumbnail;
     image.alt = "No Image Available";
 
     book.appendChild(document.createElement("hr"));
-    titleLink.appendChild(title);
-    book.appendChild(titleLink);
-    authorLink.appendChild(author);
-    book.appendChild(authorLink);
-    publisherLink.appendChild(publisher);
-    book.appendChild(publisherLink);
-    imgLink.appendChild(image);
-    book.appendChild(imgLink);
-    document.getElementById("content").appendChild(book);
+
+    book.appendChild(title);
+    title.appendChild(titleLink);
+
+    book.appendChild(author);
+    author.appendChild(authorLink);
+
+    book.appendChild(publisher);
+    publisher.appendChild(publisherLink);
+
+    imageLink.appendChild(image);
+    book.appendChild(imageLink);
+
+    section.appendChild(book);
   });
+}
+
+function handleResponse(response) {
+  "use strict";
+  var section = document.querySelector('section');
+
+  if (response.totalItems === 0) {
+    section.innerHTML = "No Results Found";
+  } else {
+    displayResults(response, section);
+  }
 }
 
 function bookSearch() {
   "use strict";
+
+  // The maximum number of results the API will return is 40
+  const MAXDISPLAYSIZE = 15;
+
   var script = document.createElement("script");
   var searchCriteria = document.getElementById("bookInput").value;
-  script.src = "https://www.googleapis.com/books/v1/volumes?q=" + searchCriteria + "&callback=handleResponse";
+  script.src = "https://www.googleapis.com/books/v1/volumes?q=" + searchCriteria + "&maxResults=" + MAXDISPLAYSIZE + "&callback=handleResponse";
   document.body.appendChild(script);
 }
+
+const ENTERKEY = 13;
 
 document.getElementById("bookInput").addEventListener("keyup", function (event) {
   "use strict";
   event.preventDefault();
-  if (event.keyCode === 13) {
+  if (event.keyCode === ENTERKEY) {
       document.getElementById("bookSearch").click();
   }
 });
